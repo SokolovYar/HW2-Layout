d0c32356375d107ff9dd158024ea185617c60fe2
SokolovYar <sokolov_ya@proton.me>
2023-12-17 11:36:31 +0200

    Add project files.

---
+++ WpfApp1/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    internal class Video
+    {
+        protected Image Image { get; set; }
+        protected string Name { get; set; }
+        protected string Author {  get; set; }
+        protected string ? Description { get; set; }
+        protected TimeOnly Duration { get; set; }
+    }
+}
